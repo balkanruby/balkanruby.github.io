b12222d1af12f6e97476a19155a991b104ebc287
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-05-02 14:12:40 +0300

    Keep the menu order in the non-landing pages the same

--- layouts/Header.js
+++ layouts/Header.js
@@ -5,17 +5,16 @@ export default () => (
   <div className="site-header-wrapper">
     <Container theme="purxle-bg" spacing="none">
       <div className="container">
-        <div className="site-header inverxted-text-bg">
+        <div className="site-header">
           <a href="/" className="logo">
             <img src="/static/assets/balkanruby-logo.svg" alt="" className="logo" />
           </a>
 
           <nav className="lead-navigation">
-            <a href="/blog/introducing-generator">Venue</a>
             <a href="/speakers">Speakers</a>
             <a href="/sponsors">Partners</a>
             <a href="/blog">Blog</a>
-            <a href="/tickets">Tickets</a>
+            <a href="/blog/introducing-generator">Venue</a>
           </nav>
         </div>
       </div>
