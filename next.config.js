9b4b65418df23ac563412f25dcdc116179000e9e
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-05-13 20:40:04 +0300

    Roll-out the community tickets for real

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       '/blog/announcement': { page: '/blog/announcement' },
       '/blog/cfp-time': { page: '/blog/cfp-time' },
       '/blog/introducing-generator': { page: '/blog/introducing-generator' },
+      '/blog/community-tickets': { page: '/blog/community-tickets' },
       '/sponsors': { page: '/sponsors' },
       '/speakers': { page: '/speakers' },
       '/blog': { page: '/blog' },
