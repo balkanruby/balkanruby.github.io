57b09fac23deb03a04a4761df09f9794f053f386
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-02-19 21:22:35 +0200

    Preserve it all on 2018.balkanruby.com

--- patterns/Breadcrumbs.js
+++
@@ -1,9 +0,0 @@
-export default () => (
-  <div className="container">
-    <nav className="breadcrumbs">
-      <a href="#">Home</a>
-      <a href="#">Section</a>
-      <span>Page</span>
-    </nav>
-  </div>
-);
--- routes.js
+++
@@ -1,7 +0,0 @@
-const routes = (module.exports = require('next-routes')());
-
-routes
-  .add('typography')
-  .add('forms')
-  .add('components')
-  .add('pages');
